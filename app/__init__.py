05f26727bc5ea5de5dfd7f3cd3c80329b26bc408
MaksHalych <galych0222@gmail.com>
2023-11-30 23:58:32 +0200

    refactoring

--- app/__init__.py
+++ app/__init__.py
@@ -3,90 +3,46 @@ from flask_smorest import Api
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 
-from routes import auth_blueprint
-from db import db
+from .routes import auth_blueprint
+from .db import db
+from config import DevelopmentConfig
 
 
-def create_app():
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
-    app.config["API_TITLE"] = "Quenic REST API"
-    app.config["API_VERSION"] = "v1"
-    app.config["OPENAPI_VERSION"] = "3.0.3"
-    app.config["OPENAPI_URL_PREFIX"] = "/"
-    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger"
-    app.config[
-        "OPENAPI_SWAGGER_UI_URL"
-    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config.from_object(config_class)
     db.init_app(app)
     migrate = Migrate(app, db)
     api = Api(app)
 
-    app.config["JWT_SECRET_KEY"] = "jose"
-    jwt = JWTManager(app)
-
-    config_jwt(jwt)
+    config_jwt(app)
     config_error_handlers(app)
-
-    app.config['SECRET_KEY'] = '9c9c2ff463c47cea625cf6652b9b2043'
 
     api.register_blueprint(auth_blueprint)
 
     return app
 
 
-def config_jwt(jwt):
-    # for JWT tokens
+def config_jwt(app):
+    jwt = JWTManager(app)
+
     @jwt.expired_token_loader
-    def expired_token_callback(jwt_header, jwt_payload):
-        return (
-            jsonify({"message": "The token has expired.", "error": "token_expired"}),
-            401,
-        )
+    def expired_token_callback():
+        response = jsonify({"message": "The token has expired"})
+        response.status_code = 401
+        return response
 
     @jwt.invalid_token_loader
-    def invalid_token_callback(error):
-        return (
-            jsonify(
-                {"message": "Signature verification failed.", "error": "invalid_token"}
-            ),
-            401,
-        )
+    def invalid_token_callback():
+        response = jsonify({"message": "Signature verification failed"})
+        response.status_code = 401
+        return response
 
     @jwt.unauthorized_loader
-    def missing_token_callback(error):
-        return (
-            jsonify(
-                {
-                    "description": "Request does not contain an access token.",
-                    "error": "authorization_required",
-                }
-            ),
-            401,
-        )
-
-    @jwt.needs_fresh_token_loader
-    def token_not_fresh_callback(jwt_header, jwt_payload):
-        return (
-            jsonify(
-                {
-                    "description": "The token is not fresh.",
-                    "error": "fresh_token_required",
-                }
-            ),
-            401,
-        )
-
-    @jwt.revoked_token_loader
-    def revoked_token_callback(jwt_header, jwt_payload):
-        return (
-            jsonify(
-                {"description": "The token has been revoked.", "error": "token_revoked"}
-            ),
-            401,
-        )
+    def missing_token_callback():
+        response = jsonify({"message": "Request does not contain an access token"})
+        response.status_code = 401
+        return response
 
 
 def config_error_handlers(app):
