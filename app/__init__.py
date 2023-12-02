ad5feb4a06fcf629ee140f8442f9d1fd436b3ab5
MaksHalych <galych0222@gmail.com>
2023-12-02 12:24:56 +0200

    fixed jwt callbacks bug

--- app/__init__.py
+++ app/__init__.py
@@ -60,19 +60,19 @@ def config_jwt(app):
     jwt = JWTManager(app)
 
     @jwt.expired_token_loader
-    def expired_token_callback():
+    def expired_token_callback(jwt_header, jwt_payload):
         response = jsonify({"message": "The token has expired"})
         response.status_code = 401
         return response
 
     @jwt.invalid_token_loader
-    def invalid_token_callback():
+    def invalid_token_callback(error):
         response = jsonify({"message": "Signature verification failed"})
         response.status_code = 401
         return response
 
     @jwt.unauthorized_loader
-    def missing_token_callback():
+    def missing_token_callback(error):
         response = jsonify({"message": "Request does not contain an access token"})
         response.status_code = 401
         return response
