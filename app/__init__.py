2c2d68a098408356cb6aae2257c977410d99a225
MaksHalych <galych0222@gmail.com>
2023-12-02 12:46:49 +0200

    fixed admin email bug

--- app/__init__.py
+++ app/__init__.py
@@ -33,13 +33,13 @@ def create_app(config_class=DevelopmentConfig):
 def create_admin_user(app):
     with app.app_context():
         db.create_all()  # create tables
-        if UserModel.query.filter_by(email="admin@exmple.com").first():
+        if UserModel.query.filter_by(email="admin@example.com").first():
             return
 
         admin_role = create_admin_role()
 
         hashed_password = make_hash("password")
-        new_user = UserModel(email="admin@exmple.com", username="admin", password=hashed_password)
+        new_user = UserModel(email="admin@example.com", username="admin", password=hashed_password)
 
         new_user.roles.append(admin_role)
 
